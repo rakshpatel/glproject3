4c527433255eb588d801e3cf1a81ab30c8dcd9da
Rakhit Patel <raksh.patel@gmail.com>
2021-09-12 12:31:43 +0530

    Updated Cognito Config

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-2_8Ypin4cTI', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '64k7hh5s4mqbpehnp32bv14ik2', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-2' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
